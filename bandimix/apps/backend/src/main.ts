75d657fc5376ef166f12c3079eeca2703c6ec707
Bandi86 <susutechno@gmail.com>
2025-06-18 11:03:28 +0200

    set up db and main bandimix

--- bandimix/apps/backend/src/main.ts
+++ bandimix/apps/backend/src/main.ts
@@ -1,10 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config'
+import { ConsoleLogger } from '@nestjs/common';
 
 declare const process: any;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: new ConsoleLogger({
+      prefix: 'NestJS',
+      timestamp: true,
+      logLevels: ['log', 'error', 'warn', 'debug', 'verbose'],
+    })
+  });
+
+  // Get the ConfigService
+  const configService = app.get(ConfigService);
+
   await app.listen(process.env.PORT ?? 8000);
+
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
